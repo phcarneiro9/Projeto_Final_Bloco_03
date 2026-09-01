0ddf8c7b88c7eaece7d79fdf097e6e7351c5cd74
phcarneiro9 <ph.carneiro9@gmail.com>
2026-09-01 15:02:41 -0300

    🐛 Corrige URL da API no frontend

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,20 +1,33 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://projeto-final-bloco-03-9i7e.onrender.com',
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    'https://projeto-final-bloco-02-bjdi.onrender.com',
 });
 
-export const buscar = async <T>(url: string, setDados: (dados: T) => void) => {
+export const buscar = async <T>(
+  url: string,
+  setDados: (dados: T) => void
+) => {
   const resposta = await api.get<T>(url);
   setDados(resposta.data);
 };
 
-export const cadastrar = async <T>(url: string, dados: object, setDados: (dados: T) => void) => {
+export const cadastrar = async <T>(
+  url: string,
+  dados: object,
+  setDados: (dados: T) => void
+) => {
   const resposta = await api.post<T>(url, dados);
   setDados(resposta.data);
 };
 
-export const atualizar = async <T>(url: string, dados: object, setDados: (dados: T) => void) => {
+export const atualizar = async <T>(
+  url: string,
+  dados: object,
+  setDados: (dados: T) => void
+) => {
   const resposta = await api.put<T>(url, dados);
   setDados(resposta.data);
 };
